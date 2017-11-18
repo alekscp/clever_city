b4604f28f6fdbbc63acc528484328dbea946ed4f
Aleksandre Clapin-Pepin <aclapinpepin@gmail.com>
2017-11-18 16:16:01 +0100

    Add image_name column to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118131649) do
+ActiveRecord::Schema.define(version: 20171118151313) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20171118131649) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "group"
+    t.string "image_name"
   end
 
   create_table "retails", force: :cascade do |t|
