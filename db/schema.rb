c0674f4419337c3010a640d673a81c733e4164a5
Aleksandre Clapin-Pepin <aclapinpepin@gmail.com>
2017-11-18 14:18:03 +0100

    Add group column to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118124711) do
+ActiveRecord::Schema.define(version: 20171118131649) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20171118124711) do
     t.integer "inventory"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "group"
   end
 
   create_table "retails", force: :cascade do |t|
