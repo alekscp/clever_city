fd79d1844098233f573f9bf6dd569b05327a26a6
Aleksandre Clapin-Pepin <aclapinpepin@gmail.com>
2017-11-19 12:34:35 +0100

    Try to fix frontend

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require popper
+//= require bootstrap-sprockets
 //= require turbolinks
-//= require_tree .
 //= require bootstrap
+//= require_tree .
